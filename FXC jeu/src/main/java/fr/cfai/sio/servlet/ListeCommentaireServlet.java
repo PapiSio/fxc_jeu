6fb00616e8a79f4c4f4bc0e3fcb4927cd820c798
mmarechet <mathieu.marechet@sfr.fr>
2016-12-07 14:39:00 +0100

    az

--- FXC jeu/src/main/java/fr/cfai/sio/servlet/ListeCommentaireServlet.java
+++ FXC jeu/src/main/java/fr/cfai/sio/servlet/ListeCommentaireServlet.java
@@ -2,13 +2,10 @@ package fr.cfai.sio.servlet;
 
 import java.io.IOException;
 import java.util.List;
-
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import fr.cfai.sio.business.Commentaire;
 import fr.cfai.sio.service.CommentaireService;
 import fr.cfai.sio.service.impl.CommentaireServiceImpl;
@@ -16,7 +13,6 @@ import fr.cfai.sio.service.impl.CommentaireServiceImpl;
 /**
  * Servlet implementation class ListeCommentaireServlet
  */
-@WebServlet("/ListeCommentaireServlet")
 public class ListeCommentaireServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private CommentaireService commentaireServiceImpl;
