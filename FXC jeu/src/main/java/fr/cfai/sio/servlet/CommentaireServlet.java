cc9f787c4b2678798ac334fec556c178091ce200
mmarechet <mathieu.marechet@sfr.fr>
2017-01-20 23:14:35 +0100

    Ajout des Servlet dans le webxml

--- FXC jeu/src/main/java/fr/cfai/sio/servlet/CommentaireServlet.java
+++ FXC jeu/src/main/java/fr/cfai/sio/servlet/CommentaireServlet.java
@@ -3,7 +3,6 @@ package fr.cfai.sio.servlet;
 import java.io.IOException;
 import java.util.Date;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,7 +12,6 @@ import fr.cfai.sio.service.impl.CommentaireServiceImpl;
 /**
  * Servlet implementation class CommentaireServlet
  */
-@WebServlet("/CommentaireServlet")
 public class CommentaireServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private CommentaireService commentaireServiceImpl;
