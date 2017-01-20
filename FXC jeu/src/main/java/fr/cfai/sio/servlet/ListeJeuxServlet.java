b1ddbc744399d8f9a56be8c46f5628b1fddae637
mmarechet <mathieu.marechet@sfr.fr>
2017-01-20 21:39:34 +0100

    Résolution porbleme affichage liste des jeux

--- FXC jeu/src/main/java/fr/cfai/sio/servlet/ListeJeuxServlet.java
+++ FXC jeu/src/main/java/fr/cfai/sio/servlet/ListeJeuxServlet.java
@@ -6,9 +6,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import javax.servlet.http.HttpSession;
-
 import fr.cfai.sio.business.Jeu;
 import fr.cfai.sio.service.JeuService;
 import fr.cfai.sio.service.TestService;
@@ -18,7 +16,8 @@ import fr.cfai.sio.service.impl.TestServiceImpl;
 /**
  * Servlet implementation class ListeJeuxServlet
  */
-public class ListeJeuxServlet extends HttpServlet {
+public class ListeJeuxServlet extends HttpServlet
+{
 	private static final long serialVersionUID = 1L;
 
 	private JeuService jeuServiceImpl;
@@ -29,7 +28,8 @@ public class ListeJeuxServlet extends HttpServlet {
 	 * @throws Exception
 	 * @see HttpServlet#HttpServlet()
 	 */
-	public ListeJeuxServlet() throws Exception {
+	public ListeJeuxServlet() throws Exception
+	{
 		System.out.println("Constructeur ListeJeuxServlet");
 
 		this.jeuServiceImpl = new JeuServiceImpl();
@@ -41,12 +41,13 @@ public class ListeJeuxServlet extends HttpServlet {
 	 *      response)
 	 */
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
 
 		System.out.println("ListeJeuxServlet - Passe par le doGet");
 
-		if (request.getParameter("action") != null) {
+		if (request.getParameter("action") != null)
+		{
 			HttpSession session = request.getSession(true);
 			int idUtilisateur = (int) session.getAttribute("ID");
 			request.setAttribute("idUtilisateur", idUtilisateur);
@@ -54,14 +55,20 @@ public class ListeJeuxServlet extends HttpServlet {
 			request.setAttribute("ListeJeu", listeJeux);
 			request.getRequestDispatcher("/ajoutTest.jsp").forward(request, response);
 
-		} else {
+		}
+		else
+		{
 
-			if (listeJeux == null) {
+			if (listeJeux == null)
+			{
 				// System.out.println("Servlet : Passe par le if, liste null");
 				listeJeux = jeuServiceImpl.recupererListeJeux();
 				request.setAttribute("LISTE_JEUX", listeJeux);
 				request.getRequestDispatcher("/listeJeux.jsp").forward(request, response);
-			} else {
+			}
+			else
+			{
+				request.setAttribute("LISTE_JEUX", listeJeux);
 				request.getRequestDispatcher("/listeJeux.jsp").forward(request, response);
 			}
 		}
@@ -72,8 +79,8 @@ public class ListeJeuxServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
 	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
 
 		System.out.println("ListeJeuxServlet - Passe par le doPost");
 
